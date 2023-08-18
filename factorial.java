99ae6167ba6537287724f17ed8cb8a9a50b6bc91
Alan Acosta <alanomar460@gmail.com>
2023-08-17 23:20:36 -0600

    programas tarea 1

---
+++ factorial.java
@@ -0,0 +1,21 @@
+/*
+ * Realice un programa en JAVA que calcule el factorial de un numero natural n incluyendo 
+ * el cero usando un ciclo for decremental, n puede ser una variable fija dentro del programa.
+ */
+
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.print("Ingrese el valor de n para calcular su factorial: ");
+        int n = sc.nextInt(), factorial = 1;
+
+        for(int aux = n; aux > 1; aux --) {
+            factorial *= aux;
+        }
+
+        System.out.println("El factorial de " + n + " es: " + factorial);
+    }
+}
---
+++ suma_n_naturales.java
@@ -0,0 +1,18 @@
+// Realice un programa en JAVA que calcule la suma de los primeros n números naturales, donde n sea un parámetro que se recibe del teclado. 
+
+import java.util.Scanner;
+
+public class suma_n_naturales {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Ingrese el valor de n: ");
+        int n = sc.nextInt(), suma = 0;
+
+        for(int i = 1; i <= n; i ++) {
+            suma += i;
+        }
+
+        System.out.println("El valor de la suma de los primeros " + n + " naturales es: " + suma);
+    }
+}
