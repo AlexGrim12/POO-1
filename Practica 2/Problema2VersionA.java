c4df1837d9044fca0577df3de5e439c8d93a8cad
Alan Acosta <alanomar460@gmail.com>
2023-09-02 19:18:32 -0600

    P2 Pr2 Version A

--- Practica 2/Problema2VersionA.java
+++ Practica 2/Problema2VersionA.java
@@ -1,21 +1,16 @@
 public class Problema2VersionA {
-    final static String[] Numeros = new String[] {
-        "Una", "Dos", "Tres", "Cuatro", 
-        "Cinco", "Seis", "Siete", "Ocho",
-        "Nueve", "Diez", "Once", "Doce"
-    };
-
-    final static String[] Regalos = new String[] {
-        "perdiz en un peral", "t\u00F3rtolas", "gallinas francesas",
-        "p\u00E1jaros piando", "anillos de oro", "ocas empollando",
-        "cisnes nadando", "sirvientas orde\u00F1ando", "damas danzando",
-        "se\u00F1ores saltando", "gaiteros tocando la gaita", 
-        "tamborileros tocando el tambor"
+    final static String[] ESTROFA = new String[] {
+        "Una perdiz en un peral", "Dos t\u00F3rtolas", 
+        "Tres gallinas francesas","Cuatro p\u00E1jaros piando", 
+        "Cinco anillos de oro", "Seis ocas empollando",
+        "Siete cisnes nadando", "Ocho sirvientas orde\u00F1ando", 
+        "Nueve damas danzando", "Diez se\u00F1ores saltando", 
+        "Once gaiteros tocando la gaita", "Doce tamborileros tocando el tambor"
     };
 
     static void estrofas(int i, int j) {
         if (j < i) {
-            System.out.println(Numeros[j] + " " + Regalos[j]);    
+            System.out.println(ESTROFA[j]);    
             estrofas(i, j + 1);
         } else
         System.out.println();
