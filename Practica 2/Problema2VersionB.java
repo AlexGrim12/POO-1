194b981aaf561bbb003aaeeff58870b5a07b82c4
Alan Acosta <alanomar460@gmail.com>
2023-09-02 19:18:41 -0600

    P2 Pr2 Version B

--- Practica 2/Problema2VersionB.java
+++ Practica 2/Problema2VersionB.java
@@ -1,18 +1,13 @@
 import java.util.Random;
 
 public class Problema2VersionB {
-    final static String[] Numeros = new String[] {
-        "Una", "Dos", "Tres", "Cuatro", 
-        "Cinco", "Seis", "Siete", "Ocho",
-        "Nueve", "Diez", "Once", "Doce"
-    };
-
-    final static String[] Regalos = new String[] {
-        "perdiz en un peral", "t\u00F3rtolas", "gallinas francesas",
-        "p\u00E1jaros piando", "anillos de oro", "ocas empollando",
-        "cisnes nadando", "sirvientas orde\u00F1ando", "damas danzando",
-        "se\u00F1ores saltando", "gaiteros tocando la gaita", 
-        "tamborileros tocando el tambor"
+    final static String[] ESTROFA = new String[] {
+        "Una perdiz en un peral", "Dos t\u00F3rtolas", 
+        "Tres gallinas francesas","Cuatro p\u00E1jaros piando", 
+        "Cinco anillos de oro", "Seis ocas empollando",
+        "Siete cisnes nadando", "Ocho sirvientas orde\u00F1ando", 
+        "Nueve damas danzando", "Diez se\u00F1ores saltando", 
+        "Once gaiteros tocando la gaita", "Doce tamborileros tocando el tambor"
     };
 
     static Integer[] Arr = new Integer[12]; 
@@ -41,7 +36,7 @@ public class Problema2VersionB {
 
     static void estrofas(int i, int j) {
         if (j < i) {
-            System.out.println(Numeros[Arr[j]] + " " + Regalos[Arr[j]]);    
+            System.out.println(ESTROFA[Arr[j]]);    
             estrofas(i, j + 1);
         } else
         System.out.println();
