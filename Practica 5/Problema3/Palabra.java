8ec84e05100e114adb944862566ec3977270ef9c
Alan Acosta <alanomar460@gmail.com>
2023-09-22 12:32:51 -0600

    mejora a la clase palabra

--- Practica 5/Problema3/Palabra.java
+++ Practica 5/Problema3/Palabra.java
@@ -1,53 +1,51 @@
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Palabra {
-    private ArrayList<String> words = new ArrayList<String>();
     private StringBuilder word = new StringBuilder();
     private StringBuilder hiddenWord = new StringBuilder();
     private Random rand = new Random();
 
     public Palabra() {
-        setWords();
-        
-        int r = rand.nextInt(words.size());
-        this.word = new StringBuilder(words.get(r).toUpperCase());
+        int r = rand.nextInt(30);
+        this.word = new StringBuilder(chooseWord(r).toUpperCase());
         
         for(int i = 0; i < word.length(); i++)
             this.hiddenWord.append('_');
     }
 
-    private void setWords() {
-        words.add("personas");
-        words.add("computadora");
-        words.add("ciudad");
-        words.add("sistema");
-        words.add("historia");
-        words.add("internet");
-        words.add("perro");
-        words.add("gato");
-        words.add("casa");
-        words.add("dia");
-        words.add("noche");
-        words.add("importante");
-        words.add("pelicula");
-        words.add("musica");
-        words.add("libro");
-        words.add("desarrollo");
-        words.add("tiempo");
-        words.add("programacion");
-        words.add("cuerpo");
-        words.add("problema");
-        words.add("solucion");
-        words.add("informacion");
-        words.add("movimiento");
-        words.add("lenguaje");
-        words.add("actividad");
-        words.add("luz");
-        words.add("fuerza");  
-        words.add("nuevo");
-        words.add("espacio");
-        words.add("naturaleza");
+    private String chooseWord(int r) {
+        switch(r) {
+            case 1: return "personas";
+            case 2: return "computadora";
+            case 3: return "ciudad";
+            case 4: return "sistema";
+            case 5: return "historia";
+            case 6: return "internet";
+            case 7: return "perro";
+            case 8: return "gato";
+            case 9: return "casa";
+            case 10: return "dia";
+            case 11: return "noche";
+            case 12: return "importante";
+            case 13: return "pelicula";
+            case 14: return "musica";
+            case 15: return "libro";
+            case 16: return "desarrollo";
+            case 17: return "tiempo";
+            case 18: return "programacion";
+            case 19: return "cuerpo";
+            case 20: return "problema";
+            case 21: return "solucion";
+            case 22: return "informacion";
+            case 23: return "movimiento";
+            case 24: return "lenguaje";
+            case 25: return "actividad";
+            case 26: return "luz";
+            case 27: return "fuerza";
+            case 28: return "nuevo";
+            case 29: return "espacio";
+            default: return "naturaleza";
+        }
     }
 
     public StringBuilder getWord() {
