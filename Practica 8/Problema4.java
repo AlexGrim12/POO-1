05efb76870fbc6c8d1b91f67d54bb7b645734dc5
Alan Acosta <alanomar460@gmail.com>
2023-10-23 21:58:07 -0600

    uso de funciones lambda

--- Practica 8/Problema4.java
+++ Practica 8/Problema4.java
@@ -1,71 +1,50 @@
+import java.util.ArrayList;
+
 public class Problema4{
     public class Sort{
-        public static String sortByLength(String s, String a){
+        private static ArrayList<String> split(String s) {
             String[] words = s.split(" ");
+            ArrayList<String> wordsAL = new ArrayList<>();
+            for (int i = 0; i < words.length; i++)
+                wordsAL.add(words[i]);
+            return wordsAL;
+        }
 
-            for (int i = 0; i < words.length; i++) {
-                for (int j = 0; j < words.length - 1; j++) {
-                    if (words[j].length() > words[j + 1].length())
-                        swap(words, j, j + 1);
-                }
-            }
+        private static String append(ArrayList<String> words){
+            String result = "";
+            for (int i = 0; i < words.size(); i++)
+                result += words.get(i) + " ";
+            return result;
+        }
 
+        public static String sortByLength(String s){
+            ArrayList<String> words = new ArrayList<>();
+            words = split(s);
+            words.sort((word1, word2) -> word1.length() - word2.length());
             return append(words);
         }
 
-        public static String sortByLength(String s, char a){
-            String[] words = s.split(" ");
+        public static String sortByLength(String s, String a){
+            ArrayList<String> words = new ArrayList<>();
+            words = split(s);
+            words.sort((word1, word2) -> word2.length() - word1.length());
+            return append(words);
+        }
 
-            for (int i = 0; i < words.length; i++) {
-                for (int j = 0; j < words.length - 1; j++) {
-                    if (words[j].length() < words[j + 1].length())
-                        swap(words, j, j + 1);
-                }
-            }
-
+        public static String sortByAlphabet(String s){
+            ArrayList<String> words = new ArrayList<>();
+            words = split(s);
+            words.sort((word1, word2) -> word1.toLowerCase().charAt(0) 
+                - word2.toLowerCase().charAt(0));
             return append(words);
         }
 
         public static String sortByAlphabet(String s, String a){
-            String[] words = s.split(" ");
-
-            for (int i = 0; i < words.length; i++) {
-                for (int j = 0; j < words.length - 1; j++) {
-                    if (words[j].toLowerCase().charAt(0) 
-                        > words[j + 1].toLowerCase().charAt(0))
-                        swap(words, j, j + 1);
-                }
-            }
-
+            ArrayList<String> words = new ArrayList<>();
+            words = split(s);
+            words.sort((word1, word2) -> word2.toLowerCase().charAt(0) 
+                - word1.toLowerCase().charAt(0));
             return append(words);
-        }
-
-        public static String sortByAlphabet(String s, char a){
-            String[] words = s.split(" ");
-
-            for (int i = 0; i < words.length; i++) {
-                for (int j = 0; j < words.length - 1; j++) {
-                    if (words[j].toLowerCase().charAt(0) 
-                        < words[j + 1].toLowerCase().charAt(0))
-                        swap(words, j, j + 1);
-                }
-            }
-
-            return append(words);
-        }
-
-        private static void swap(String[] words, int i, int j){
-            String temp = words[i];
-            words[i] = words[j];
-            words[j] = temp;
-        }
-
-        private static String append(String[] words){
-            String result = "";
-            for (int i = 0; i < words.length; i++)
-                result += words[i] + " ";
-
-            return result;
         }
     }
 
@@ -75,11 +54,11 @@ public class Problema4{
         System.out.println("String: " + s);
         
         System.out.println("\n- Ordenacion por longitud -");
-        System.out.println("Ascendente: " + Sort.sortByLength(s, "s"));
-        System.out.println("Descendente: " + Sort.sortByLength(s, 's'));
+        System.out.println("Ascendente: " + Sort.sortByLength(s));
+        System.out.println("Descendente: " + Sort.sortByLength(s, "s"));
         
         System.out.println("\n- Ordenacion alfabetica -");
-        System.out.println("a-z: " + Sort.sortByAlphabet(s, "s"));
-        System.out.println("z-a: " + Sort.sortByAlphabet(s, 's'));
+        System.out.println("a-z: " + Sort.sortByAlphabet(s));
+        System.out.println("z-a: " + Sort.sortByAlphabet(s, "s"));
     }
 }
